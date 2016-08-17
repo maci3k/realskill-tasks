c66b5921aeef7bc40495f90d10c6b6ac189d678a
Maciej Klimczyk <jackson93.07@gmail.com>
2016-08-17 12:45:12 +0200

    Task completed

--- app/timeConversion.js
+++ app/timeConversion.js
@@ -3,6 +3,45 @@
 
   window.TimeUtils = {
     convertTo12HoursTime: function (time) {
+      var tab = time.split(':', 3);
+      var hour, minute, second;
+      var hourAfter;
+      var suffix;
+
+      if(tab.length !== 3) {
+        throw new Error();
+      }
+
+      hour = tab[0];
+      minute = tab[1];
+      second = tab[2];
+
+      if(hour.length !== 2 || minute.length !==2 || second.length !==2) {
+        throw new Error();
+      }
+
+      if(hour <= '11') {
+        hourAfter = hour;
+        suffix = 'A.M.';
+      }
+
+      if(hour === '00') {
+        hourAfter = '12';
+        suffix = 'A.M.';
+      }
+
+      if(hour === '12') {
+        hourAfter = '12';
+        suffix = 'P.M.';
+      }
+
+      if(hour > '12') {
+        hourAfter = hour % '12';
+        suffix = 'P.M.';
+      }
+
+      time = hourAfter + ':' + minute + ':' + second + ' ' + suffix;
+
       return time;
     }
   };
